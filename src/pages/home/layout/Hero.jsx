204b370c81b70553d866e55cdafd71b24a4f8561
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:25:57 -0500

    estructuracion del hero

--- src/pages/home/layout/Hero.jsx
+++ src/pages/home/layout/Hero.jsx
@@ -1,5 +1,11 @@
 export function Hero(){
     return <div>
-        
+        <div>
+            <h1>FrontEnd</h1>
+            <h2>Challenge React</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, neque.
+            </p>
+        </div>
     </div>
 }
