8ab95eec295c970115d271a19857de96144407b2
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 16:35:18 -0500

    prop publicar los estilos

--- src/global/components/Dialog.jsx
+++ src/global/components/Dialog.jsx
@@ -1,6 +1,8 @@
-export function Dialog({children}) {
+export function Dialog({ children, className }) {
 	return (
-		<div className="absolute left-0 bottom-0 -translate-x-1/2 translate-y-1/2">
+		<div
+			className={`absolute left-0 bottom-0 -translate-x-1/2 translate-y-1/2 ${className}`}
+		>
 			{children}
 		</div>
 	);
