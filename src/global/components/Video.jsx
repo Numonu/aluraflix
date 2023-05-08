a9cf5e8a224fb246d8847a61003fc16e3b53db58
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:48:30 -0500

    pruebas de video con la api de youtube

--- src/global/components/Video.jsx
+++ src/global/components/Video.jsx
@@ -1,5 +1,15 @@
-export function Video(){
-    return <div>
-        
-    </div>
+export function Video() {
+	return (
+		<div>
+            <iframe
+				width="560"
+				height="315"
+				src="https://www.youtube-nocookie.com/embed/7tEbuCZNVeM"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowfullscreen
+			></iframe>
+        </div>
+	);
 }
