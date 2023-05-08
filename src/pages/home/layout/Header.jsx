4e68e66401a33a81c984d9c74fb0959060f80e8b
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 16:26:35 -0500

    cambie el boton unico por un dialog

--- src/pages/home/layout/Header.jsx
+++ src/pages/home/layout/Header.jsx
@@ -1,8 +1,9 @@
+import { BsThreeDotsVertical } from "react-icons/bs";
 export function Header(){
-    return <header className="bg-zinc-900 py-2 flex justify-between">
+    return <header className="bg-zinc-900 py-2 flex justify-between items-center">
         <img className="w-32" src="images/logo.png" alt="Logo de aluraflix" />
-        <button className="text-white border-white px-2 border-2 hover:bg-white hover:text-black transition-colors">
-            Nuevo Video
+        <button className="text-white px-1 aspect-square text-2xl hover:bg-white hover:text-black transition-colors rounded-full xl:text-xl">
+            <BsThreeDotsVertical/>
         </button>
     </header>
 }
