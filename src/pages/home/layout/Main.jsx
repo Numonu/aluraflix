9bb382d5fd75f9489ce820c8f595791464805361
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 12:29:18 -0500

    setup para un fondo de pantalla completa

--- src/pages/home/layout/Main.jsx
+++ src/pages/home/layout/Main.jsx
@@ -3,8 +3,10 @@ import { Hero } from "./Hero";
 export function Main() {
 	return (
 		<main>
-			<div className=" bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_1280.jpg)]">
-				<Hero />
+			<div className="bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_1280.jpg)]">
+                <div className="max-w-[1200px] mx-auto">
+				    <Hero />
+                </div>
 			</div>
 		</main>
 	);
