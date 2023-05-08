257d31497c51411b1b1ad3c5d3fa636bb2cbf898
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 12:28:00 -0500

    se quito del wrapper al hero

--- src/pages/home/layout/Home.jsx
+++ src/pages/home/layout/Home.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./Header";
 import { Main } from "./Main";
 
-export function Home(){
-    return <div className="text-neutral-100">
-        <div className="bg-zinc-900 px-2">
-            <div className="max-w-[1200px] mx-auto">
-                <Header/>
-                <Main/>
-            </div>
-        </div>
-    </div>
+export function Home() {
+	return (
+		<div className="text-neutral-100">
+			<div className="bg-zinc-900 px-2">
+				<div className="max-w-[1200px] mx-auto">
+					<Header />
+				</div>
+			</div>
+			<Main />
+		</div>
+	);
 }
--- src/pages/home/layout/Main.jsx
+++ src/pages/home/layout/Main.jsx
@@ -1,7 +1,11 @@
 import { Hero } from "./Hero";
 
-export function Main(){
-    return <main>
-        <Hero/>
-    </main>
+export function Main() {
+	return (
+		<main>
+			<div className=" bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_1280.jpg)]">
+				<Hero />
+			</div>
+		</main>
+	);
 }
