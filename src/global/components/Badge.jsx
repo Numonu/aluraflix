d7adbee6c64e0601f1964ddb163219ed37f4f8c7
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:39:56 -0500

    reduccion del bold en la fuente

--- src/global/components/Badge.jsx
+++ src/global/components/Badge.jsx
@@ -1,5 +1,5 @@
 export function Badge({children , className}){
-    return <div className={`w-max px-3 py-1 rounded-md font-medium text-3xl ${className}`}>
+    return <div className={`w-max px-3 py-1 rounded-md text-3xl ${className}`}>
         {children}
     </div>  
 }
