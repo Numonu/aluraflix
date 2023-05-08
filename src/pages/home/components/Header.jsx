b06dae80bcc55582bbc1c6a14ee85872d9aea867
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:12:51 -0500

    estilizacion del logo y el boton

--- src/pages/home/components/Header.jsx
+++ src/pages/home/components/Header.jsx
@@ -1,5 +1,8 @@
 export function Header(){
-    return <div>
-        
-    </div>
+    return <header className="bg-zinc-900 p-2 flex justify-between">
+        <img className="w-32" src="images/logo.png" alt="Logo de aluraflix" />
+        <button className="text-white border-white px-2 border-2 hover:bg-white hover:text-black transition-colors">
+            Nuevo Video
+        </button>
+    </header>
 }
