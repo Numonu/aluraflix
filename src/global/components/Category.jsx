e345026a89ad574686e5e414b352b5e87dffd11f
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 13:17:55 -0500

    setup del componente de carousel

--- src/global/components/Category.jsx
+++ src/global/components/Category.jsx
@@ -1,5 +1,11 @@
+import { Badge } from "./Badge";
+import { Carousel } from "./Carousel";
+
 export function Category(){
     return <div>
-        
+        <Badge>Back End</Badge>
+        <Carousel>
+            
+        </Carousel>
     </div>
 }
