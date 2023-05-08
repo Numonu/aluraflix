b26591276ff907e03c79509abbfa7f7ad84540c6
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 10:38:59 -0500

    setup de frameworks y librerias

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./styles/main.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 className='text-xl text-emerald-500'>Hello World</h1>
+  </React.StrictMode>,
+)
