fa8078e0999bd816edcec9953076bf8de62334fd
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:19:05 -0500

    setup del wrapper del header

--- src/pages/home/layout/Home.jsx
+++ src/pages/home/layout/Home.jsx
@@ -1,7 +1,11 @@
 import { Header } from "../components/Header";
 
 export function Home(){
-    return <div>
-        <Header/>
+    return <div className="text-neutral-100">
+        <div className="bg-zinc-900 px-4">
+            <div className="max-w-[1200px] mx-auto">
+                <Header/>
+            </div>
+        </div>
     </div>
 }
