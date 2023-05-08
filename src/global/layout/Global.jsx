7c0b3f11c8fcf1ffdbb4e5cd8d9b29053d746447
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 18:04:26 -0500

    movi el componente dialog y navDialog al la carpeta global

--- src/global/layout/Global.jsx
+++ src/global/layout/Global.jsx
@@ -1,5 +1,7 @@
+import { Header } from "./Header";
+
 export function Global(){
     return <div>
-        
+        <Header/>
     </div>
 }
--- src/global/layout/Header.jsx
+++ src/global/layout/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { NavDialog } from "../../pages/home/components/NavDialog";
+import { NavDialog } from "../components/NavDialog";
 export function Header() {
 	const [dialog, setDialog] = useState(false);
 
