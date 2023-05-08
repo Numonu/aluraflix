70b44fccf177d5ee5ac9aa18c2bac61633aae6d3
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:30:59 -0500

    ajuste en el padding

--- src/pages/home/layout/Hero.jsx
+++ src/pages/home/layout/Hero.jsx
@@ -1,11 +1,17 @@
-export function Hero(){
-    return <div>
-        <div>
-            <h1>FrontEnd</h1>
-            <h2>Challenge React</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, neque.
-            </p>
-        </div>
-    </div>
+export function Hero() {
+	return (
+		<div className="py-4 px-1">
+			<div>
+				<h1>FrontEnd</h1>
+				<h2>Challenge React</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Eius, neque.
+				</p>
+			</div>
+			<div>
+
+            </div>
+		</div>
+	);
 }
