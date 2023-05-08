037f9260e62ac61ea4cb8bec24fa2da22b972ef3
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 14:02:35 -0500

    porp de link para renderizar los videos

--- src/global/components/Video.jsx
+++ src/global/components/Video.jsx
@@ -1,14 +1,18 @@
-export function Video({className}) {
+export function Video({ className, link }) {
+
+	const url = new URL(link || "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+	const videoId = url.searchParams.get("v");
+
 	return (
 		<div className={`border-2 ${className}`}>
-            <iframe
-                className="w-full aspect-video"
-				src="https://www.youtube-nocookie.com/embed/7tEbuCZNVeM"
+			<iframe
+				className="w-full aspect-video"
+				src={`https://www.youtube-nocookie.com/embed/${videoId}`}
 				title="YouTube video player"
 				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowFullscreen
+				allowFullScreen
 			></iframe>
-        </div>
+		</div>
 	);
 }
