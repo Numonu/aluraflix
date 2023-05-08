c1bb41daeaa912d9be01e321b8ea2d5b1195022b
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:49:45 -0500

    render del componente de video

--- src/pages/home/layout/Hero.jsx
+++ src/pages/home/layout/Hero.jsx
@@ -1,4 +1,5 @@
 import { Badge } from "../../../global/components/Badge";
+import { Video } from "../../../global/components/Video";
 
 export function Hero() {
 	return (
@@ -16,7 +17,7 @@ export function Hero() {
 				</p>
 			</div>
 			<div>
-
+                <Video/>
             </div>
 		</div>
 	);
