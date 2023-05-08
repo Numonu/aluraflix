a82df6c590463631861144433b36f80398182381
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:34:29 -0500

    uso del badge en el titulo

--- src/pages/home/layout/Hero.jsx
+++ src/pages/home/layout/Hero.jsx
@@ -1,8 +1,12 @@
+import { Badge } from "../../../global/components/Badge";
+
 export function Hero() {
 	return (
 		<div className="py-4 px-1">
 			<div>
-				<h1>FrontEnd</h1>
+				<Badge>
+                    FrontEnd
+                </Badge>
 				<h2>Challenge React</h2>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
