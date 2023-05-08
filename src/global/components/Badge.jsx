63c31df5f48fc07663cd8ef588e61d36f3cdc69b
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 15:36:14 -0500

    reduccion del tamaño de fuente de los badges

--- src/global/components/Badge.jsx
+++ src/global/components/Badge.jsx
@@ -1,7 +1,7 @@
 export function Badge({ children, className, style }) {
 	return (
 		<div
-			className={`w-max px-3 py-1 rounded-md text-3xl ${className}`}
+			className={`w-max px-3 py-1 rounded-md text-2xl ${className}`}
 			style={style}
 		>
 			{children}
