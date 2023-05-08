cdf469fa640640ab82d27328c124546affa0d3f0
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 13:15:12 -0500

    carousel scroll setup

--- src/global/components/Carousel.jsx
+++ src/global/components/Carousel.jsx
@@ -1,7 +1,8 @@
 import { Video } from "./Video";
 
 export function Carousel(){
-    return <div>
-        <Video/>
+    return <div className="w-full pb-4 flex overflow-y-hidden overflow-scroll gap-4">
+        <Video className="shrink-0"/>
+        <Video className="shrink-0"/>
     </div>
 }
--- src/pages/home/layout/Main.jsx
+++ src/pages/home/layout/Main.jsx
@@ -1,12 +1,20 @@
+import { Carousel } from "../../../global/components/Carousel";
 import { Hero } from "./Hero";
 
 export function Main() {
 	return (
 		<main>
-			<div className="bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_1280.jpg)]">
-                <div className="max-w-[1200px] mx-auto">
-				    <Hero />
-                </div>
+			<div className="bg-cover bg-[url(https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_1280.jpg)] px-2">
+				<div className="max-w-[1200px] mx-auto">
+					<Hero />
+				</div>
+			</div>
+			<div className="bg-zinc-900 px-2 py-6">
+				<div className="max-w-[1200px] mx-auto">
+					<div className="">
+						<Carousel />
+					</div>
+				</div>
 			</div>
 		</main>
 	);
