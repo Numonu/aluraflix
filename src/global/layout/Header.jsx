382e5aa48fa81ef4b1537c5db7dcfcfb99da048c
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 18:26:32 -0500

    con el logo podemos volver a la pagina principal

--- src/global/layout/Header.jsx
+++ src/global/layout/Header.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { NavDialog } from "../components/NavDialog";
+import { useNavigate } from "react-router-dom";
 export function Header() {
 	const [dialog, setDialog] = useState(false);
+	const navigate = useNavigate();
 
 	const open = () => setDialog(true);
 	const close = () => setDialog(false);
@@ -10,9 +12,10 @@ export function Header() {
 	return (
 		<header className="py-2 px-2 flex justify-between items-center">
 			<img
-				className="w-32"
+				className="w-32 cursor-pointer"
 				src="images/logo.png"
 				alt="Logo de aluraflix"
+				onClick={() => navigate("/")}
 			/>
 			<button
 				className="text-white relative px-1 aspect-square text-2xl hover:bg-white hover:text-black transition-colors rounded-full xl:text-xl"
