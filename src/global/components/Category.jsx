308f8998a7d3870b12e19f150a03a2feac473ba0
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 14:58:22 -0500

    exposicion del prop para incluir videos en una categoria

--- src/global/components/Category.jsx
+++ src/global/components/Category.jsx
@@ -1,13 +1,13 @@
 import { Badge } from "./Badge";
 import { Carousel } from "./Carousel";
 
-export function Category({ title, badgeBackground }) {
+export function Category({ videos, title, badgeBackground }) {
 	return (
 		<div>
 			<Badge style={{ backgroundColor: badgeBackground }}>
 				{title || "Empty"}
 			</Badge>
-			<Carousel />
+			<Carousel videos={videos}/>
 		</div>
 	);
 }
