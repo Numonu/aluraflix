9cba50aabe8a0717d6f76d6557772c8c54e52196
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:36:56 -0500

    estilizacion del badge

--- src/global/components/Badge.jsx
+++ src/global/components/Badge.jsx
@@ -1,5 +1,5 @@
 export function Badge({children , className}){
-    return <div className={`${className}`}>
+    return <div className={`w-max px-3 py-1 rounded-md font-medium text-3xl ${className}`}>
         {children}
     </div>  
 }
