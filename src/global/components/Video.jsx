d5239b3c373ba04100b65c05fa7460fe3522e4e5
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 11:52:28 -0500

    ajustes en los estilos y propiedades de los videos

--- src/global/components/Video.jsx
+++ src/global/components/Video.jsx
@@ -2,13 +2,12 @@ export function Video() {
 	return (
 		<div>
             <iframe
-				width="560"
-				height="315"
+                className="w-full aspect-video"
 				src="https://www.youtube-nocookie.com/embed/7tEbuCZNVeM"
 				title="YouTube video player"
-				frameborder="0"
+				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowfullscreen
+				allowFullscreen
 			></iframe>
         </div>
 	);
