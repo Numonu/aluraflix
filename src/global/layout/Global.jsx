3692b819928f05c7eaf37cf89e0201cef34897c0
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 18:08:49 -0500

    padding individuales por componente

--- src/global/layout/Global.jsx
+++ src/global/layout/Global.jsx
@@ -2,7 +2,7 @@ import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
 
 export function Global(){
-    return <div>
+    return <div className="text-neutral-50">
         <Header/>
         <Outlet/>
     </div>
--- src/global/layout/Header.jsx
+++ src/global/layout/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
 	const close = () => setDialog(false);
 
 	return (
-		<header className="bg-zinc-900 py-2 flex justify-between items-center">
+		<header className="bg-zinc-900 py-2 px-2 flex justify-between items-center">
 			<img
 				className="w-32"
 				src="images/logo.png"
