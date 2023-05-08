a0cd12ff579967b8f5d75124491cc60c67325baf
Numonu <juan.villegas.dev@gmail.com>
2023-05-08 16:31:32 -0500

    setup de estado para el dialog del header

--- src/pages/home/layout/Header.jsx
+++ src/pages/home/layout/Header.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-export function Header(){
-    return <header className="bg-zinc-900 py-2 flex justify-between items-center">
-        <img className="w-32" src="images/logo.png" alt="Logo de aluraflix" />
-        <button className="text-white px-1 aspect-square text-2xl hover:bg-white hover:text-black transition-colors rounded-full xl:text-xl">
-            <BsThreeDotsVertical/>
-        </button>
-    </header>
+export function Header() {
+	const [dialog, setDialog] = useState(false);
+
+	const open = () => setDialog(true);
+	const close = () => setDialog(false);
+
+	return (
+		<header className="bg-zinc-900 py-2 flex justify-between items-center">
+			<img
+				className="w-32"
+				src="images/logo.png"
+				alt="Logo de aluraflix"
+			/>
+			<button
+				className="text-white relative px-1 aspect-square text-2xl hover:bg-white hover:text-black transition-colors rounded-full xl:text-xl"
+				onClick={() => (dialog ? close() : open())}
+			>
+                <div className="absolute left-0 bottom-0 -translate-x-1/2 translate-y-1/2">
+                    xd
+                </div>
+				<BsThreeDotsVertical />
+			</button>
+		</header>
+	);
 }
