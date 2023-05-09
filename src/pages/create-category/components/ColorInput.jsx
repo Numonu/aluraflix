c26869dc56f67f556e9e4846f7212a1e5ae7382e
Numonu <juan.villegas.dev@gmail.com>
2023-05-09 10:25:48 -0500

    simplificacion del input de color

--- src/pages/create-category/components/ColorInput.jsx
+++ src/pages/create-category/components/ColorInput.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-
-export function ColorInput() {
-	const [color, setColor] = useState("");
+export function ColorInput({onColor}) {
 	return (
 		<label htmlFor="color" className="text-xl mt-6 flex items-center gap-4">
 			<span>Color de la categoria</span>
 			<input
-            style={{backgroundColor : color}}
-				className="bg-transparent border-2 border-white"
 				type="color"
-				id="color"
-				onInput={(e) => setColor(e.target.value)}
+				onInput={e => onColor(e.target.value)}
 			/>
 		</label>
 	);
