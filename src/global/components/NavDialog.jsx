9d5abfba60cdeb16f9ef0911d342cda76b2d1855
Numonu <juan.villegas.dev@gmail.com>
2023-05-10 11:38:17 -0500

    navegacion a la home

--- src/global/components/NavDialog.jsx
+++ src/global/components/NavDialog.jsx
@@ -1,5 +1,5 @@
 import { Dialog } from "./Dialog";
-import { AiOutlineVideoCameraAdd , AiOutlineFolderOpen } from "react-icons/ai";
+import { AiOutlineVideoCameraAdd , AiOutlineFolderOpen , AiOutlineHome } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 export function NavDialog() {
 	const navigate = useNavigate();
@@ -7,15 +7,21 @@ export function NavDialog() {
 		<Dialog className="bg-black text-white p-2 text-lg flex flex-col gap-2 w-max text-start rounded-md lg:text-base">
 			<div
 				className="hover:bg-white hover:text-black p-1 flex gap-2 items-center"
+				onClick={() => navigate("/")}
+			>
+				<AiOutlineHome className="text-xl"/> Inicio
+			</div>
+			<div
+				className="hover:bg-white hover:text-black p-1 flex gap-2 items-center"
 				onClick={() => navigate("/create-video")}
 			>
-				<AiOutlineVideoCameraAdd className="text-xl"/> New Video
+				<AiOutlineVideoCameraAdd className="text-xl"/> Nuevo Video
 			</div>
 			<div
 				className="hover:bg-white hover:text-black p-1 flex gap-2 items-center"
 				onClick={() => navigate("/create-category")}
 			>
-				<AiOutlineFolderOpen className="text-xl"/> New Category
+				<AiOutlineFolderOpen className="text-xl"/> Nueva Categoria
 			</div>
 		</Dialog>
 	);
